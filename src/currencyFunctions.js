1058611941fe9a4de5bfc228bb9b45b55c76ba03
NomadiCxac <christianxavier.cordero@gmail.com>
2023-05-29 12:23:20 -0400

    Added function to display player current currency to UI

--- src/currencyFunctions.js
+++ src/currencyFunctions.js
@@ -1,12 +1,10 @@
 import GGTokenImage from "./images/GGToken.png"
-import KeyImage from "./images/Key.png"
+import ChestKeyImage from "./images/ChestKey.png"
 
-
-const validCurrencies = ["GGTokens", "Keys"]
-
+const validCurrencies = ["GGTokens", "ChestKeys"]
 const currencyImages = {
     GGTokens: GGTokenImage,
-    Keys: KeyImage
+    ChestKeys: ChestKeyImage
 };
 
 export const rewardUtilities = {
@@ -17,7 +15,6 @@ export const rewardUtilities = {
         const rewardType = quest.reward.type;
 
         if (validCurrencies.includes(rewardType)) {
-            console.log(GGTokenImage )
             return currencyImages[rewardType];
         }
 
@@ -25,6 +22,17 @@ export const rewardUtilities = {
     return null;
   }
 };
+
+export function displayPlayerCurrentCurrency (currencyContainer) {
+    for (let index in currencyContainer) {
+        let currencyInterface = document.querySelector(`#${currencyContainer[index].type}Container`);
+        let currencyAmount = document.createElement("div");
+        currencyAmount.textContent = (`: ${currencyContainer[index].amount} ${currencyContainer[index].type}`);
+        currencyAmount.classList.add("currencyAmountUserInterface");
+        currencyAmount.setAttribute("id", `${currencyContainer[index].type}UserInterface`);
+        currencyInterface.appendChild(currencyAmount);
+    }
+}
 
 
 export function currencyAggregator (currencyContainer, currentQuest) {
--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import { displayFormModal, closeFormModal } from "./modalfunctions.js";
 import dueDate from "./dueDate.js";
 import getObjective from "./getObjective.js";
 import currencyAggregator from "./currencyAggregator.js";
-import GGTokens from "../src/images/GGToken.png"
-import Keys from "../src/images/ChestKey.png"
+import { displayPlayerCurrentCurrency } from './currencyFunctions';
 
 
 // Globally Scoped Variables
 let currentQuestList = [];
-let currencyContainer = [new Currency("GGTokens", 0), new Currency("Keys", 0)]
+let currencyContainer = [new Currency("GGTokens", 0), new Currency("ChestKeys", 0)]
 var currentDate = new Date();
 var year = currentDate.getFullYear();
 var month = String(currentDate.getMonth() + 1).padStart(2, '0');
@@ -23,20 +22,22 @@ questForm.setAttribute("value", formattedDate);
 
 
 // test cases
-let gymTask = new Quest(getObjective("Gym"), dueDate(20, 30, 0), false, new Currency("Keys", 2));
+let gymTask = new Quest(getObjective("Gym"), dueDate(20, 30, 0), false, new Currency("ChestKeys", 2));
 let waterTask = new Quest(getObjective("Water"), dueDate(20, 30, 0), false, new Currency("GGTokens", 15));
 addQuest(currentQuestList, gymTask);
 addQuest(currentQuestList, waterTask);
 (currencyAggregator(currencyContainer, gymTask));
 (currencyAggregator(currencyContainer, waterTask));
 createAndDisplayQuestCards(currentQuestList);
-
+console.log(currentQuestList);
 
 // Event Listener to Open Quest Creation Modal
 let addQuestButtonClicked = document.querySelector("button#addQuestButton")
 addQuestButtonClicked.addEventListener("click", function () {
     displayFormModal();
 })
+
+displayPlayerCurrentCurrency(currencyContainer);
 
 
 // Event Listener to Add Quest to Quest List and Display
