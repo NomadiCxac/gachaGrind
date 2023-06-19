2f62b7086961aa4249fd3e70056afea60bb79a3b
NomadiCxac <christianxavier.cordero@gmail.com>
2023-06-19 16:52:42 -0400

    added item modal to show item image on card

--- src/inventoryFunctions.js
+++ src/inventoryFunctions.js
@@ -3,28 +3,37 @@ import { getWeaponImage, getArmourImage, getElementImage, getRarityImage } from 
 const inventoryPageParent = document.querySelector(".gameContent");
 const inventoryPage = document.createElement("div");
 
-export function createInventoryPage () {
+export function createInventoryPage (inventory) {
     
     inventoryPage.classList.add("inventoryPage")
-    generateInventoryItemElements()
+    generateInventoryItemElements(inventory)
     inventoryPageParent.appendChild(inventoryPage)
 }
 
-function generateInventoryItemElements () {
+// This is separate from inventory and only generates the container for inventory items
+function generateInventoryItemElements (inventory) {
 
+    // Code to generate each inventory row
     for (let i = 0; i < 5; i ++) {
         let inventoryItemRow = document.createElement("div");
         inventoryItemRow.classList.add("inventoryItemRow");
         inventoryItemRow.setAttribute("id", `inventoryItemRow-${i+1}`);
         inventoryPage.appendChild(inventoryItemRow)
         let counter = (i * 5);
+
+        // Code to generate each index in each inventory row
         for (let j = 0; j < 5; j++) {
             let inventoryItem = document.createElement("div");
             inventoryItem.classList.add("inventoryItem");
-            inventoryItem.setAttribute("id", `inventoryItem-${counter + j + 1}`);
+            inventoryItem.setAttribute("id", `${counter + j + 1}`);
             inventoryItem.addEventListener("click", function(e) {
-                console.log(e.target);
-                showAndPositionInventoryModal(e.target);
+                let inventoryIndex = (e.target.id - 1);
+                if (e.target.style.backgroundImage == "") {
+                    return;
+                } else {
+                    generateItemCardModal(e.target);
+                    appendItemImage(inventory);
+                }
             })
             inventoryItem.addEventListener("mouseover", function() {
                 inventoryItem.style.border = "4px solid yellow";
@@ -41,25 +50,49 @@ function generateInventoryItemElements () {
 export function appendItemImage (inventory) {
     console.log("This is working");
     for (let item = 0; item < inventory.length; item++) {
-        let itemContainer = document.querySelector(`#inventoryItem-${item + 1}`);
+        let itemContainer = document.querySelectorAll('.inventoryItem')[item];
         let itemImage = getWeaponImage(inventory[item]._type.replace(/\s/g, ''));
+        console.log(itemImage)
         itemContainer.style.backgroundImage = `url('${itemImage}')`
         itemContainer.addEventListener("mouseover", function() {
-            console.log(`This is item${inventory[item]._name}`);
+            console.log(inventory[item]);
         }
     )};
 }
 
 
-export function createInventoryModal() {
+export function createInventoryModal(e, inventory) {
+
+ 
   const inventoryModal = document.createElement("div");
   inventoryModal.classList.add("inventory-modal");
 
   const inventoryModalContent = document.createElement("div");
   inventoryModalContent.classList.add("inventory-modal-content");
 
+  const itemCardTopHalf = document.createElement("div");
+  itemCardTopHalf.classList.add("itemCardTopHalf");
+  const itemCardBottomHalf = document.createElement("div");
+  itemCardBottomHalf.classList.add("itemCardBottomHalf");
+
   const inventoryModalItemImage = document.createElement("div");
-  inventoryModalItemImage.classList.add("inventory-modal-item-image")
+  inventoryModalItemImage.classList.add("inventory-modal-item-image");
+  let itemImage = e.style.backgroundImage;
+  inventoryModalItemImage.style.backgroundImage = itemImage;
+
+  const inventoryModalItemStats = document.createElement("div");
+  inventoryModalItemStats.classList.add("inventory-modal-item-stats");
+
+  const elementDescription = document.createElement("p");
+//   let itemElement = inventoryItem.weapon.element;
+  elementDescription.innerText = "Element: Volt";
+
+  const rarityDescription = document.createElement("p");
+//   let itemRarity = inventoryItem.weapon.element;
+  rarityDescription.innerText = "Rarirty: Legendary";
+
+  const damageDescription = document.createElement("p");
+  damageDescription.innerText = "Weapon Damage: 8422.00";
 
   const closeBtn = document.createElement("button");
   closeBtn.classList.add("inventory-close");
@@ -76,7 +109,15 @@ export function createInventoryModal() {
 
   inventoryModalContent.appendChild(closeBtn);
   inventoryModalContent.appendChild(inventoryModalTitle);
-  inventoryModalContent.appendChild(inventoryModalItemImage);
+  inventoryModalContent.appendChild(itemCardTopHalf);
+  inventoryModalContent.appendChild(itemCardBottomHalf);
+  itemCardTopHalf.appendChild(inventoryModalItemImage);
+  itemCardTopHalf.appendChild(inventoryModalItemStats);
+
+  inventoryModalItemStats.appendChild(elementDescription);
+  inventoryModalItemStats.appendChild(rarityDescription);
+  inventoryModalItemStats.appendChild(damageDescription);
+
   inventoryModalContent.appendChild(inventoryModalContentText);
 
   inventoryModal.appendChild(inventoryModalContent);
@@ -85,8 +126,8 @@ export function createInventoryModal() {
   return inventoryModal;
 }
 
-export function showAndPositionInventoryModal() {
-  const inventoryModal = createInventoryModal();
+export function generateItemCardModal(e) {
+  const inventoryModal = createInventoryModal(e);
   inventoryModal.style.display = "block";
 }
 
@@ -95,3 +136,12 @@ export function hideInventoryModal(e) {
     inventoryModal.style.display = "none";
     inventoryModal.remove();
   }
+
+function generateItemCard(item) {
+    const itemImage = getWeaponImage(item);
+    console.log(itemImage);
+}
+
+function getItemImage (inventory) {
+    
+}
